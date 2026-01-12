c920f6fc1998ace7903c75b74de1ea32d0c539f0
kbg1513 <kbgarcia1513@gmail.com>
2026-01-12 11:12:05 +0000

    Initial login page

--- frontend/src/components/pages/LoginPage/LoginPage.styles.ts
+++ frontend/src/components/pages/LoginPage/LoginPage.styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { v } from 'constants/variables';
 import { Link } from "react-router-dom";
+import { DynamicForm } from '@kbgarcia8/react-dynamic-form';
 
 export const LoginPageWrapper = styled.div`
     padding: ${v.spacing.large};
@@ -15,8 +16,51 @@ export const LoginPageWrapper = styled.div`
 export const LoginPageHeader =  styled.h2`
     width: 100%;
     text-align: center;
+`;
 
-
+export const StyledDynamicForm = styled(DynamicForm)`
+    background-color: ${({theme})=> theme.colors.bg};
+    color: ${({theme})=> theme.colors.text};
+    &.with-fieldsets,
+    &.without-fieldsets{
+        height: auto;
+        padding: 1.25rem;
+    }
+    & .address-fieldset-wrapper{
+        display: flex;
+        flex-direction: column;
+    }
+    & fieldset{
+        flex: 1;
+        padding: 1rem;
+        width: auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    & legend {
+        font-weight: 700;
+    }
+    & .address-field-label-n-input-container {
+        width: 75%;
+        padding: 0.5rem;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    & .add-input-button-space{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    & .add-input-entry {
+        width: 12.5%;
+    }
+    
+    & .education-form-label-n-input-container {
+        width: 75%;
+        padding: 0.5rem;
+        justify-content: space-between;
+    }
 `;
 
 export const SignUpMessageSpace = styled.div`
