12d0ec4af5288008f2a1a64faa5ba9033e764aa4
kbgarcia8 <kbgarcia8@gmail.com>
2026-02-06 00:04:05 +0800

    authentication integration test - signup. FOR final test

--- api/src/modules/auth/tests/auth.integration.test.ts
+++ api/src/modules/auth/tests/auth.integration.test.ts
@@ -1,216 +1,239 @@
 import request from "supertest";
 import { describe, it, expect } from "vitest";
 import { app } from "../../../app";
+import prisma from "lib/prisma";
+import { User } from "prisma/schema/generated/prisma";
 
-describe("POST /auth/signup/local", () => {
-    it("ERROR: should not allowed non-existent email", async () => {
-        const res = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "kbgarcia@test.com",
-            firstname: "Karl",
-            lastname: "Garcia",
-            username: "",
-            password: "@Thisisatest1234",
-            confirmpassword: "@Thisisatest1234",
+describe("Auth module", () => {
+    let token: string;
+    let userId: string;
+
+    describe("Signup", () => {
+        it("ERROR: should not allowed non-existent email", async () => {
+            const res = await request(app)
+            .post("/auth/signup/local")
+            .send({
+                email: "kbgarcia@test.com",
+                firstname: "Karl",
+                lastname: "Garcia",
+                username: "",
+                password: "@Thisisatest1234",
+                confirmpassword: "@Thisisatest1234",
+            });
+
+            expect(res.status).toBe(400);
+            expect(res.body.message).toBe('Email not valid/deliverable/existent');
         });
 
-        console.log("Response body:", res.body);
+        it("ERROR: email is required", async () => {
+            const res = await request(app)
+            .post("/auth/signup/local")
+            .send({
+                email: "",
+                firstname: "Karl",
+                lastname: "Garcia",
+                username: "",
+                password: "@Thisisatest1234",
+                confirmpassword: "@Thisisatest1234",
+            });
 
-        expect(res.status).toBe(400);
-        expect(res.body.message).toBe('Email not valid/deliverable/existent');
+            expect(res.status).toBe(400);
+            expect(res.body.message).toMatch(/Email is required!|Email not valid/);
+        });
+
+        it("ERROR: firstname is required", async () => {
+            const res = await request(app)
+            .post("/auth/signup/local")
+            .send({
+                email: "kbgarcia1513@gmail.com",
+                firstname: "",
+                lastname: "Garcia",
+                username: "",
+                password: "@Thisisatest1234",
+                confirmpassword: "@Thisisatest1234",
+            });
+
+            expect(res.status).toBe(400);
+            expect(res.body.message).toContain('First Name is required!');
+        });
+
+        it("ERROR: lastname is required", async () => {
+            const res = await request(app)
+            .post("/auth/signup/local")
+            .send({
+                email: "kbgarcia1513@gmail.com",
+                firstname: "Karl",
+                lastname: "",
+                username: "",
+                password: "@Thisisatest1234",
+                confirmpassword: "@Thisisatest1234",
+            });
+
+            expect(res.status).toBe(400);
+            expect(res.body.message).toContain('Last Name is required!');
+        });
+
+        it("ERROR: invalid username characters", async () => {
+            const res = await request(app)
+            .post("/auth/signup/local")
+            .send({
+                email: "kbgarcia1513@gmail.com",
+                firstname: "Karl",
+                lastname: "Garcia",
+                username: "!@#$%^sadfasfjh0901928",
+                password: "@Thisisatest1234",
+                confirmpassword: "@Thisisatest1234",
+            });
+
+            expect(res.status).toBe(400);
+            expect(res.body.message).toContain('Username must contain alphanumeric characters only!');
+
+            const res2 = await request(app)
+            .post("/auth/signup/local")
+            .send({
+                email: "kbgarcia1513@gmail.com",
+                firstname: "Karl",
+                lastname: "Garcia",
+                username: "asd",
+                password: "@Thisisatest1234",
+                confirmpassword: "@Thisisatest1234",
+            });
+
+            expect(res2.status).toBe(400);
+            expect(res2.body.message).toContain('Username must be atleast 5 characters and at max 35 characters!');
+
+            const res3 = await request(app)
+            .post("/auth/signup/local")
+            .send({
+                email: "kbgarcia1513@gmail.com",
+                firstname: "Karl",
+                lastname: "Garcia",
+                username: "asdasdwqe4233erewfdfst242135134eetertertrewtrewtwertwert24332432432432rewfdsfdfsrewtret",
+                password: "@Thisisatest1234",
+                confirmpassword: "@Thisisatest1234",
+            });
+
+            expect(res3.status).toBe(400);
+            expect(res3.body.message).toContain('Username must be atleast 5 characters and at max 35 characters!');
+        });
+
+        it("ERROR: password is required, password is invalid", async () => {
+            const res = await request(app)
+            .post("/auth/signup/local")
+            .send({
+                email: "kbgarcia1513@gmail.com",
+                firstname: "Karl",
+                lastname: "Garcia",
+                username: "",
+                password: "",
+                confirmpassword: "",
+            });
+
+            expect(res.status).toBe(400);
+            expect(res.body.message).toContain('Please provide a password!');
+
+            const res2 = await request(app)
+            .post("/auth/signup/local")
+            .send({
+                email: "kbgarcia1513@gmail.com",
+                firstname: "Karl",
+                lastname: "Garcia",
+                username: "",
+                password: "asd",
+                confirmpassword: "asd",
+            });
+
+            expect(res2.status).toBe(400);
+            expect(res2.body.message).toContain('Password must be at least 8 characters and include uppercase, lowercase, and a symbol');
+        });
+
+        it("ERROR: confirmpassword is required", async () => {
+            const res = await request(app)
+            .post("/auth/signup/local")
+            .send({
+                email: "kbgarcia1513@gmail.com",
+                firstname: "Karl",
+                lastname: "Garcia",
+                username: "",
+                password: "@Thisisatest1234",
+                confirmpassword: "",
+            });
+
+            expect(res.status).toBe(400);
+            expect(res.body.message).toContain('Please confirm password!');
+        });
+
+        it("SUCCESS: should create a user", async () => {
+            const data = {
+                email: "kbgarcia1513@gmail.com",
+                firstname: "Karl",
+                lastname: "Garcia",
+                username: "",
+                password: "@Thisisatest1234",
+                confirmpassword: "@Thisisatest1234",
+            }
+
+            const res = await request(app)
+            .post("/auth/signup/local")
+            .send(data);
+
+            expect(res.status).toBe(200);
+            expect(res.body.data.user.email).toBe("kbgarcia1513@gmail.com");
+            expect(res.body.data.user.isVerified).toBe(false);
+            expect(res.body.message).toBe('User signup successful. Please verify email');
+
+            const user = await prisma.user.findUnique({
+                where: { email: data.email},
+                include: {verificationTokens: true}
+            });
+
+            token = user?.verificationTokens[0]?.token!;
+            userId = user?.id!;
+        });
+
+        it("ERROR: should not allow used email", async () => {
+            const res = await request(app)
+            .post("/auth/signup/local")
+            .send({
+                email: "kbgarcia1513@gmail.com",
+                firstname: "Karl",
+                lastname: "Garcia",
+                username: "",
+                password: "@Thisisatest1234",
+                confirmpassword: "@Thisisatest1234",
+            });
+
+            expect(res.status).toBe(500);
+            expect(res.body.code).toBe("P2002");
+            expect(res.body.message).toBe("Email/username already in use");
+        });
     });
 
-    it("ERROR: email is required", async () => {
-        const res = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "",
-            firstname: "Karl",
-            lastname: "Garcia",
-            username: "",
-            password: "@Thisisatest1234",
-            confirmpassword: "@Thisisatest1234",
+    describe("Verify", () => {
+        it("SUCCESS: Verifies the email", async() => {
+            const res = await request(app)
+            .get("/auth/verify")
+            .query({ token })
+
+            expect(res.status).toBe(200);
+            expect(res.body.isVerified).toBe(true);
         });
 
-        console.log("Response body:", res.body);
+        it("ERROR: Token is expired", async() => {
+            const expiredToken = await prisma.verificationToken.create({
+                data: {
+                    token: "expired-token-123",
+                    expiresAt: new Date(Date.now() - 1000 * 60 * 60),
+                    userId: userId
+                }
+            });
 
-        expect(res.status).toBe(400);
-        expect(res.body.message).includes('Email is required!');
-    });
+            const res = await request(app)
+                .get("/auth/verify")
+                .query({ token: expiredToken.token });
 
-    it("ERROR: firstname is required", async () => {
-        const res = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "kbgarcia1513@gmail.com",
-            firstname: "",
-            lastname: "Garcia",
-            username: "",
-            password: "@Thisisatest1234",
-            confirmpassword: "@Thisisatest1234",
+            expect(res.status).toBe(401);
+            expect(res.body.message).toContain("Token expired");
         });
-
-        console.log("Response body:", res.body);
-
-        expect(res.status).toBe(400);
-        expect(res.body.message).includes('First Name is required!');
-    });
-
-    it("ERROR: lastname is required", async () => {
-        const res = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "kbgarcia1513@gmail.com",
-            firstname: "Karl",
-            lastname: "",
-            username: "",
-            password: "@Thisisatest1234",
-            confirmpassword: "@Thisisatest1234",
-        });
-
-        console.log("Response body:", res.body);
-
-        expect(res.status).toBe(400);
-        expect(res.body.message).includes('Last Name is required!');
-    });
-
-    it("ERROR: invalid username characters", async () => {
-        const res = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "kbgarcia1513@gmail.com",
-            firstname: "Karl",
-            lastname: "Garcia",
-            username: "!@#$%^sadfasfjh0901928",
-            password: "@Thisisatest1234",
-            confirmpassword: "@Thisisatest1234",
-        });
-
-        console.log("Response body:", res.body);
-
-        expect(res.status).toBe(400);
-        expect(res.body.message).includes('Username must contain alphanumeric characters only!');
-
-        const res2 = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "kbgarcia1513@gmail.com",
-            firstname: "Karl",
-            lastname: "Garcia",
-            username: "asd",
-            password: "@Thisisatest1234",
-            confirmpassword: "@Thisisatest1234",
-        });
-
-        expect(res2.status).toBe(400);
-        expect(res2.body.message).includes('Username must be atleast 5 characters and at max 35 characters!');
-
-        const res3 = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "kbgarcia1513@gmail.com",
-            firstname: "Karl",
-            lastname: "Garcia",
-            username: "asdasdwqe4233erewfdfst242135134eetertertrewtrewtwertwert24332432432432rewfdsfdfsrewtret",
-            password: "@Thisisatest1234",
-            confirmpassword: "@Thisisatest1234",
-        });
-
-        expect(res3.status).toBe(400);
-        expect(res3.body.message).includes('Username must be atleast 5 characters and at max 35 characters!');
-    });
-
-    it("ERROR: password is required, password is invalid", async () => {
-        const res = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "kbgarcia1513@gmail.com",
-            firstname: "Karl",
-            lastname: "Garcia",
-            username: "",
-            password: "",
-            confirmpassword: "",
-        });
-
-        console.log("Response body:", res.body);
-
-        expect(res.status).toBe(400);
-        expect(res.body.message).includes('Please provide a password!');
-
-        const res2 = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "kbgarcia1513@gmail.com",
-            firstname: "Karl",
-            lastname: "Garcia",
-            username: "",
-            password: "asd",
-            confirmpassword: "asd",
-        });
-
-        console.log("Response body:", res.body);
-
-        expect(res2.status).toBe(400);
-        expect(res2.body.message).includes('Password must be at least 8 characters and include uppercase, lowercase, and a symbol');
-    });
-
-    it("ERROR: confirmpassword is required", async () => {
-        const res = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "kbgarcia1513@gmail.com",
-            firstname: "Karl",
-            lastname: "Garcia",
-            username: "",
-            password: "@Thisisatest1234",
-            confirmpassword: "",
-        });
-
-        console.log("Response body:", res.body);
-
-        expect(res.status).toBe(400);
-        expect(res.body.message).includes('Please confirm password!');
-    });
-/*
-    it("SUCCESS: should create a user", async () => {
-        const res = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "kbgarcia1513@gmail.com",
-            firstname: "Karl",
-            lastname: "Garcia",
-            username: "",
-            password: "@Thisisatest1234",
-            confirmpassword: "@Thisisatest1234",
-        });
-
-
-        console.log("Response body:", res.body);
-
-        expect(res.status).toBe(200);
-        expect(res.body.data.user.email).toBe("kbgarcia1513@gmail.com");
-        expect(res.body.data.user.isVerified).toBe(false);
-        expect(res.body.message).toBe('User signup successful. Please verify email');
-    });
-
-    it("ERROR: should not allow used email", async () => {
-        const res = await request(app)
-        .post("/auth/signup/local")
-        .send({
-            email: "kbgarcia1513@gmail.com",
-            firstname: "Karl",
-            lastname: "Garcia",
-            username: "",
-            password: "@Thisisatest1234",
-            confirmpassword: "@Thisisatest1234",
-        });
-
-        console.log("Response body:", res.body);
-
-        expect(res.status).toBe(500);
-        expect(res.body.code).toBe("P2002");
-        expect(res.body.message).toBe("Email/username already in use");
-    });
-    */
-});
+    })
+})
